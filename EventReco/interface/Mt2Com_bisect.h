adb7949c872dad25d8a13d5a438e0a59001bc7cb
Alexandre Aubin <alexandre.aubin@cern.ch>
2013-09-29 18:03:33 +0200

    Added another method where the lepton is directly given in input (easier support for combined1LeptonStopSelection with 2-lepton case)

--- EventReco/interface/Mt2Com_bisect.h
+++ EventReco/interface/Mt2Com_bisect.h
@@ -14,47 +14,54 @@
 
 
 class Mt2Com_bisect : public mt2bl_bisect::mt2bl, 
-											public mt2w_bisect::mt2w, 
-											public mt2_bisect::mt2
+                                            public mt2w_bisect::mt2w, 
+                                            public mt2_bisect::mt2
 {
-	public:
-	
-	Mt2Com_bisect();
-	~Mt2Com_bisect();
-	
-	double calculateMT2w(const std::vector<IPHCTree::NTJet>& jets, 
-											const std::vector<IPHCTree::NTJet>& bjets, 
-											const std::vector<IPHCTree::NTMuon>& muon, 
-											const std::vector<IPHCTree::NTElectron>& electron,
-											TVector2 vecMet,
-											string mt2type);
+    public:
+    
+    Mt2Com_bisect();
+    ~Mt2Com_bisect();
+    
+    double calculateMT2w(const std::vector<IPHCTree::NTJet>& jets, 
+                         const std::vector<IPHCTree::NTJet>& bjets, 
+                         const std::vector<IPHCTree::NTMuon>& muon, 
+                         const std::vector<IPHCTree::NTElectron>& electron,
+                         TVector2 vecMet,
+                         string mt2type);
 
-	double mt2wWrapper(TLorentzVector& lep, const TLorentzVector& jet_o,
-																		const TLorentzVector& jet_b, float& met, float& metphi,
-																		string& mt2type);
+    double calculateMT2w(const std::vector<IPHCTree::NTJet>& jets, 
+                         const std::vector<IPHCTree::NTJet>& bjets, 
+                         const TLorentzVector leptonInput, 
+                         TVector2 vecMet,
+                         string mt2type);
 
-	private:
-	
-	std::vector<IPHCTree::NTJet> jets; 
-	std::vector<IPHCTree::NTJet> bjets; 
-	std::vector<IPHCTree::NTMuon> muon; 
-	std::vector<IPHCTree::NTElectron> electron;
-	TVector2 vecMet;
-	string mt2type;
-	TLorentzVector jet_o;
-	TLorentzVector jet_b;
-	float metval;
-	float metphi;
-	TLorentzVector lep;
-	int n_btag;
-	int nMax;
-	float metx;
-	float mety;
-	double pl[4];     // Visible lepton
-	double pb1[4];    // bottom on the same side as the visible lepton
-	double pb2[4];    // other bottom, paired with the invisible W
-	double pmiss_lep[3];
-	double pmiss[3];  // <unused>, pmx, pmy   missing pT
+
+    double mt2wWrapper(TLorentzVector& lep, const TLorentzVector& jet_o,
+                       const TLorentzVector& jet_b, float& met, float& metphi,
+                       string& mt2type);
+
+    private:
+    
+    std::vector<IPHCTree::NTJet> jets; 
+    std::vector<IPHCTree::NTJet> bjets; 
+    std::vector<IPHCTree::NTMuon> muon; 
+    std::vector<IPHCTree::NTElectron> electron;
+    TVector2 vecMet;
+    string mt2type;
+    TLorentzVector jet_o;
+    TLorentzVector jet_b;
+    float metval;
+    float metphi;
+    TLorentzVector lep;
+    int n_btag;
+    int nMax;
+    float metx;
+    float mety;
+    double pl[4];     // Visible lepton
+    double pb1[4];    // bottom on the same side as the visible lepton
+    double pb2[4];    // other bottom, paired with the invisible W
+    double pmiss_lep[3];
+    double pmiss[3];  // <unused>, pmx, pmy   missing pT
 
 
 };
